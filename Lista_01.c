d86cba6ffcd7941b4f175619e93def382030c9af
DSB88 <diego.brandao@outlook.com>
2021-04-20 16:57:31 -0300

    Add Ex 03

--- Lista_01.c
+++ Lista_01.c
@@ -58,3 +58,34 @@ main()
 
 #endif ex2
 
+#ifdef ex3
+/*    3-) Leia dois valores numéricos e exibe a diferença do maior pelo menor. Se os números forem iguais informe ao usuário. */
+
+main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int  x,y,soma;
+
+    printf("Digite dois números: \n");
+    scanf("%d" "%d", &x, &y);
+
+    if (x==y)
+    {
+        printf("Os números digitados foram %d e %d são números iguais", x,y);
+    }
+    else if (x>y)
+    {
+        soma = x-y;
+        printf("A diferença entre o número digitado %d e %d é %d\n", x,y,soma);
+    }
+    else
+    {
+        soma = y-x;
+        printf("A diferença entre o número digitado %d e %d é %d\n", x,y,soma);
+    }
+
+}
+
+#endif ex3
+
