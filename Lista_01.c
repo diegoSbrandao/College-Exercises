d48177226ac882e9e079e36429c63518c2b2b133
DSB88 <diego.brandao@outlook.com>
2021-04-21 00:49:52 -0300

    Add Ex 04

--- Lista_01.c
+++ Lista_01.c
@@ -89,3 +89,26 @@ main()
 
 #endif ex3
 
+#ifdef ex4
+
+/* 4-) Receba o preço de um produto e calcular novo preço com desconto de 9%. */
+
+main()
+{
+
+    setlocale(LC_ALL, "Portuguese");
+
+    double  x,aum;
+
+    printf("Digite o valor do produto R$: \n");
+    scanf("%lf", &x);
+
+    aum=x - (x*0.09);
+
+    printf("O valor do produto com desconto de 9 porcento fica R$: %.2lf\n", aum); //Acrescentei um arredondamento de 2 casas decimais.
+
+    system("pause"); // pausa o programa
+}
+
+#endif ex4
+
