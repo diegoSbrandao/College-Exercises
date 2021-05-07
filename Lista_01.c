989e9b33c3732b47baac7f47e987271ba170bfe0
DSB88 <diego.brandao@outlook.com>
2021-05-06 22:33:12 -0300

    Add Ex 05

--- Lista_01.c
+++ Lista_01.c
@@ -112,3 +112,42 @@ main()
 
 #endif ex4
 
+
+#ifdef ex5
+/*
+
+5.	Receba dois números quaisquer, onde o primeiro deve ser maior que zero e o segundo menor ou igual a zero. Calcule e mostre na tela o produto entre eles.
+*/
+
+main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int x,y,multi;
+
+    do
+    {
+        printf("Digite dois números: \n");
+        scanf("%d" "%d", &x,&y);
+
+        if (x<0)
+        {
+            printf("Seu primeiro número deve ser maior que 0\n");
+        }
+        else if (y>0)
+        {
+            printf("Seu segundo número deve ser menor que 0 ou 0\n");
+        }
+
+    }
+    while(x<0 || y >0);
+
+    multi = x*y;
+
+    printf("A multiplicação dos números digitados %d e %d é igual: %d\n ",x,y,multi);
+    system("pause"); // pausa o programa
+
+}
+
+#endif ex5
+
