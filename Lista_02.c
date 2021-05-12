92acf5f6b6c7b8244623079ef369954dbf23826d
DSB88 <diego.brandao@outlook.com>
2021-05-12 20:29:30 -0300

    Add Ex 05 e 06

--- Lista_02.c
+++ Lista_02.c
@@ -167,3 +167,88 @@ main()
     }
 }
 #endif ex4
+
+#ifdef ex5
+main()
+{
+
+    /*
+        5.	Receba três números que representam os lados de um triângulo e garantam a existência de um triângulo.
+
+        Informe ao usuário se o triângulo é isóscele, equilátero ou escaleno.
+
+        Observações:
+        a.	Garantir que cada lado é menor que a soma dos outros dois lados.
+        b.	O triângulo é equilátero quando todos os lados são iguais.
+        c.	O triângulo é isóscele quando apenas dois lados são iguais.
+        d.	O triângulo é escaleno quando todos os lados são diferentes.
+
+   */
+
+    setlocale(LC_ALL, "Portuguese");
+
+    double  x,y,z;
+
+    do
+    {
+        printf("Digite três números: \n");
+        scanf("%lf" "%lf" "%lf",&x,&y,&z);
+
+        if(x>=y+z || y>=x+z || z>=x+y)
+        {
+            printf("Os números digitados não formam um triângulo\n");
+        }
+
+    }
+    while(x>=y+z || y>=x+z || z>=x+y);
+
+    if (x==y && x==z)
+    {
+        printf("O triângulo é equilátero."); //quando todos os lados são iguais.
+    }
+    else if (x!=y && x!=z && y!=z)
+    {
+        printf("O triângulo é escaleno."); //quando todos os lados são diferentes.
+    }
+    else
+    {
+        printf("O triângulo é isósceles."); //quando apenas dois lados são iguais.
+    }
+
+
+}
+#endif ex5
+
+#ifdef ex6
+main()
+{
+    /*
+        6.	Cada degrau de uma escada tem X de altura. Faça um programa que receba essa altura e a altura que o usuário deseja subindo a escada.
+        Calcule e mostre quantos degraus o usuário deverá subir para atingir seu objetivo, sem se preocupar com a altura do usuário.
+    */
+
+    setlocale(LC_ALL, "Portuguese");
+
+    int  x,y,deg;
+    float degraus;
+
+    do {
+            printf("Digite a altura do degrau em cm:\n");
+            scanf("%d",&x);
+
+            printf("Digite qual altura deseja subir em metros:\n");
+            scanf("%d",&y);
+
+    }while(x<=0 || y<=0 || x>y*100);
+
+    degraus =  y*100/x;
+    deg = degraus;
+
+   if(deg==degraus){
+    printf("A quantidade de degraus que terá que subir será: %.2f.",degraus);
+}
+   else{
+   printf("A quantidade de degraus que terá que subir será: %d.",deg+1);
+   }
+}
+#endif ex6
